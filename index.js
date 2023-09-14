e560bcea78eabf539f38f699e8829a69d2689c73
Scott Shannon <scott.a.shannon@outlook.com>
2023-09-14 09:23:18 -0700

    hello space

---
+++ index.js
@@ -0,0 +1,24 @@
+// var button = document.querySelector("button");
+
+// var clickAlert = function() {
+//     alert("SOMEONE CLICKED THE BUTTON!!");
+// }
+
+// button.addEventListener("click", clickAlert);
+
+
+
+// window.addEventListener("click", function() {
+//     alert("You clicked on the page!");
+//   });
+
+document.getElementById("myBtn").addEventListener("click", myFunction);
+
+function myFunction() {
+  alert ("Hello World!");
+}
+
+ 
+
+
+  
